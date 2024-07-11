624fbfd1cdcdbb29c73b206bdee293e8d1358a45
Marcel Müller <marcel.mueller@husqvarnagroup.com>
2024-07-11 18:21:05 +0200

    Firmware filenames: display full file name with size
    Version alone does not work to distinguish the different firmwares.

--- goosebit/ui/static/js/firmware.js
+++ goosebit/ui/static/js/firmware.js
@@ -88,7 +88,7 @@ function updateFirmwareList() {
 
         data.forEach(item => {
             const listItem = document.createElement('li');
-            listItem.textContent = item["version"];
+            listItem.textContent = `${item["name"]}, size: ${(item["size"] / 1024 / 1024).toFixed(2)} MB`;
             listItem.classList = ["list-group-item d-flex justify-content-between align-items-center"];
 
             const btnGroup = document.createElement("div")
