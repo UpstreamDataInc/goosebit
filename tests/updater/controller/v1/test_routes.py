62822737e3eb86ca9b8f5115778d234025c04314
Marcel Müller <marcel.mueller@husqvarnagroup.com>
2024-07-28 22:01:05 +0200

    test: validate changes through /api endpoints
    This extends the test coverage and no longer relies on internals (db state)
    to validate state changes.

--- tests/updater/controller/v1/test_routes.py
+++ tests/updater/controller/v1/test_routes.py
@@ -1,9 +1,35 @@
 import pytest
 
-from goosebit.models import Firmware, Hardware, UpdateStateEnum
+from goosebit.models import Firmware, Hardware
 from goosebit.updater.manager import get_update_manager
 
 UUID = "221326d9-7873-418e-960c-c074026a3b7c"
+
+
+async def _api_login(async_client):
+    login_data = {"username": "admin@goosebit.local", "password": "admin"}
+    response = await async_client.post("/login", data=login_data, follow_redirects=True)
+    assert response.status_code == 200
+
+
+async def _api_device_update(async_client, device, update_attribute, update_value):
+    response = await async_client.post(
+        f"/api/devices/update",
+        json={"devices": [f"{device.uuid}"], update_attribute: update_value},
+    )
+    assert response.status_code == 200
+
+
+async def _api_devices_get(async_client):
+    response = await async_client.get("/api/devices/all")
+    assert response.status_code == 200
+    return response.json()
+
+
+async def _api_rollouts_get(async_client):
+    response = await async_client.get("/api/rollouts/all")
+    assert response.status_code == 200
+    return response.json()
 
 
 async def _poll_first_time(async_client):
@@ -102,6 +128,10 @@ async def test_register_device(async_client, test_data):
 
     await _poll(async_client, UUID, None, False)
 
+    await _api_login(async_client)
+    devices = await _api_devices_get(async_client)
+    assert devices[0]["state"] == "Registered"
+
 
 @pytest.mark.asyncio
 async def test_rollout_full(async_client, test_data):
@@ -115,18 +145,20 @@ async def test_rollout_full(async_client, test_data):
 
     # confirm installation start (in reality: several of similar posts)
     await _feedback(async_client, device.uuid, firmware, "none", "proceeding")
-    await device.refresh_from_db()
-    assert device.last_state == UpdateStateEnum.RUNNING
+    await _api_login(async_client)
+    devices = await _api_devices_get(async_client)
+    assert devices[0]["state"] == "Running"
 
     # report finished installation
     await _feedback(async_client, device.uuid, firmware, "success", "closed")
-    await device.refresh_from_db()
-    assert device.last_state == UpdateStateEnum.FINISHED
-    assert device.fw_version == firmware.version
+    devices = await _api_devices_get(async_client)
+    assert devices[0]["state"] == "Finished"
+    assert devices[0]["fw"] == firmware.version
 
     await rollout.refresh_from_db()
-    assert rollout.success_count == 1
-    assert rollout.failure_count == 0
+    rollouts = await _api_rollouts_get(async_client)
+    assert rollouts[0]["success_count"] == 1
+    assert rollouts[0]["failure_count"] == 0
 
 
 @pytest.mark.asyncio
@@ -140,8 +172,9 @@ async def test_rollout_signalling_download_failure(async_client, test_data):
 
     # confirm installation start (in reality: several of similar posts)
     await _feedback(async_client, device.uuid, firmware, "none", "proceeding")
-    await device.refresh_from_db()
-    assert device.last_state == UpdateStateEnum.RUNNING
+    await _api_login(async_client)
+    devices = await _api_devices_get(async_client)
+    assert devices[0]["state"] == "Running"
 
     # HEAD /api/download/1 HTTP/1.1 (reason not clear)
     response = await async_client.head(firmware_url)
@@ -154,14 +187,17 @@ async def test_rollout_signalling_download_failure(async_client, test_data):
 
     # report failure
     await _feedback(async_client, device.uuid, firmware, "failure", "closed")
-    await device.refresh_from_db()
-    assert device.last_state == UpdateStateEnum.ERROR
+    devices = await _api_devices_get(async_client)
+    assert devices[0]["state"] == "Error"
 
 
 @pytest.mark.asyncio
 async def test_latest(async_client, test_data):
-    device = test_data["device_latest"]
+    device = test_data["device_rollout"]
     firmware = test_data["firmware_latest"]
+
+    await _api_login(async_client)
+    await _api_device_update(async_client, device, "firmware", "latest")
 
     deployment_base = await _poll(async_client, device.uuid, firmware)
 
@@ -169,19 +205,23 @@ async def test_latest(async_client, test_data):
 
     # confirm installation start (in reality: several of similar posts)
     await _feedback(async_client, device.uuid, firmware, "none", "proceeding")
-    await device.refresh_from_db()
-    assert device.last_state == UpdateStateEnum.RUNNING
+    await _api_login(async_client)
+    devices = await _api_devices_get(async_client)
+    assert devices[0]["state"] == "Running"
 
     # report finished installation
     await _feedback(async_client, device.uuid, firmware, "success", "closed")
-    await device.refresh_from_db()
-    assert device.last_state == UpdateStateEnum.FINISHED
-    assert device.fw_version == firmware.version
+    devices = await _api_devices_get(async_client)
+    assert devices[0]["state"] == "Finished"
+    assert devices[0]["fw"] == firmware.version
 
 
 @pytest.mark.asyncio
 async def test_latest_with_no_firmware_available(async_client, test_data):
-    device = test_data["device_latest"]
+    device = test_data["device_rollout"]
+
+    await _api_login(async_client)
+    await _api_device_update(async_client, device, "firmware", "latest")
 
     fake_hardware = await Hardware.create(model="does-not-exist", revision="default")
     device.hardware_id = fake_hardware.id
@@ -192,15 +232,22 @@ async def test_latest_with_no_firmware_available(async_client, test_data):
 
 @pytest.mark.asyncio
 async def test_pinned(async_client, test_data):
-    device = test_data["device_pinned"]
+    device = test_data["device_rollout"]
+
+    await _api_login(async_client)
+    await _api_device_update(async_client, device, "pinned", True)
 
     await _poll(async_client, device.uuid, None, False)
 
 
 @pytest.mark.asyncio
 async def test_up_to_date(async_client, test_data):
-    device = test_data["device_latest"]
+    device = test_data["device_rollout"]
     firmware = test_data["firmware_latest"]
+
+    await _api_login(async_client)
+    await _api_device_update(async_client, device, "firmware", "latest")
+
     manager = await get_update_manager(dev_id=device.uuid)
     await manager.update_fw_version(firmware.version)
 
