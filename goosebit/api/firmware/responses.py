3b95428c8700669203e3c293155bb08adc990286
Upstream Data <brett@upstreamdata.ca>
2024-08-08 15:16:52 -0600

    Update firmware with the same style as devices, and make typing stricter.

---
+++ goosebit/api/firmware/responses.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+import asyncio
+
+from fastapi.requests import Request
+from pydantic import BaseModel, Field
+
+from goosebit.models import Firmware
+
+from .models import FirmwareModel
+
+
+class FirmwareTableResponse(BaseModel):
+    data: list[FirmwareModel]
+    draw: int
+    records_total: int = Field(serialization_alias="recordsTotal")
+    records_filtered: int = Field(serialization_alias="recordsFiltered")
+
+    @classmethod
+    async def parse(cls, request: Request, query, search_filter, total_records):
+        params = request.query_params
+
+        draw = int(params.get("draw", 1))
+        start = int(params.get("start", 0))
+        length = int(params.get("length", 10))
+        search_value = params.get("search[value]", None)
+        order_column_index = params.get("order[0][column]", None)
+        order_column = params.get(f"columns[{order_column_index}][data]", None)
+        order_dir = params.get("order[0][dir]", None)
+
+        if search_value:
+            query = query.filter(search_filter(search_value))
+
+        if order_column:
+            query = query.order_by(f"{"-" if order_dir == "desc" else ""}{order_column}")
+
+        filtered_records = await query.count()
+        devices = await query.offset(start).limit(length).all()
+        data = list(await asyncio.gather(*[FirmwareModel.parse(d) for d in devices]))
+
+        return cls(data=data, draw=draw, records_total=total_records, records_filtered=filtered_records)
+
+
+class FirmwareAllResponse(BaseModel):
+    firmware: list[FirmwareModel]
+
+    @classmethod
+    async def parse(cls, firmware: list[Firmware]):
+        return cls(firmware=await asyncio.gather(*[FirmwareModel.parse(f) for f in firmware]))
--- goosebit/api/firmware/routes.py
+++ goosebit/api/firmware/routes.py
@@ -1,52 +1,47 @@
-from typing import Any
-
-from fastapi import APIRouter, Body, HTTPException, Security
+from fastapi import APIRouter, HTTPException, Security
 from fastapi.requests import Request
 from tortoise.expressions import Q
 
-from goosebit.api.helper import filter_data
+from goosebit.api.responses import StatusResponse
 from goosebit.auth import validate_user_permissions
 from goosebit.models import Firmware, Rollout
 from goosebit.permissions import Permissions
 
-router = APIRouter(prefix="/firmware")
+from .requests import DeleteFirmwareRequest
+from .responses import FirmwareAllResponse, FirmwareTableResponse
+
+router = APIRouter(prefix="/firmware", tags=["firmware"])
 
 
 @router.get(
     "/all",
     dependencies=[Security(validate_user_permissions, scopes=[Permissions.FIRMWARE.READ])],
 )
-async def firmware_get_all(
-    request: Request,
-) -> dict[str, int | list[dict[str, list[Any] | Any]]]:
-    query = Firmware.all()
+async def firmware_get_all(_: Request) -> FirmwareAllResponse:
+    return await FirmwareAllResponse.parse(await Firmware.all().prefetch_related("compatibility"))
 
+
+@router.get(
+    "/table",
+    dependencies=[Security(validate_user_permissions, scopes=[Permissions.FIRMWARE.READ])],
+)
+async def firmware_get_table(request: Request) -> FirmwareTableResponse:
     def search_filter(search_value):
         return Q(uri__icontains=search_value) | Q(version__icontains=search_value)
 
-    async def parse(f):
-        compatibility = await f.compatibility.all().values()
-
-        return {
-            "id": f.id,
-            "name": f.path_user,
-            "size": f.size,
-            "hash": f.hash,
-            "version": f.version,
-            "compatibility": compatibility,
-        }
-
+    query = Firmware.all().prefetch_related("compatibility")
     total_records = await Firmware.all().count()
-    return await filter_data(request, query, search_filter, parse, total_records)
+
+    return await FirmwareTableResponse.parse(request, query, search_filter, total_records)
 
 
 @router.post(
     "/delete",
     dependencies=[Security(validate_user_permissions, scopes=[Permissions.FIRMWARE.DELETE])],
 )
-async def firmware_delete(_: Request, files: list[int] = Body()) -> dict:
+async def firmware_delete(_: Request, config: DeleteFirmwareRequest) -> StatusResponse:
     success = False
-    for f_id in files:
+    for f_id in config.files:
         firmware = await Firmware.get_or_none(id=f_id)
 
         if firmware is None:
@@ -63,4 +58,4 @@ async def firmware_delete(_: Request, files: list[int] = Body()) -> dict:
 
         await firmware.delete()
         success = True
-    return {"success": success}
+    return StatusResponse(success=success)
