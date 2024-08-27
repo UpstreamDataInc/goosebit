71d8f401c3b303c558707506144667f461ffe21d
Upstream Data <brett@upstreamdata.ca>
2024-08-27 11:35:16 -0600

    Update devices routes to use BFF only
    Update API schema and BFF schema to allow UI to only use BFF for devices.

---
+++ goosebit/ui/bff/devices/requests.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from pydantic import BaseModel
+
+
+class DevicesDeleteRequest(BaseModel):
+    devices: list[str]
+
+
+class DevicesPatchRequest(BaseModel):
+    devices: list[str]
+    software: str | None = None
+    name: str | None = None
+    pinned: bool | None = None
+    feed: str | None = None
+    force_update: bool | None = None
