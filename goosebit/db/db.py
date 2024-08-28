3b4639e6ca21f6172a95b1d3cefb17dd6cac873d
Marcel Müller <marcel.mueller@husqvarnagroup.com>
2024-08-28 11:59:56 +0200

    Remove local software file cleanup
    This code can be convenient for the developer but could be dangerous when
    running in production. Deleting software will also delete linked rollouts,
    so something that should not be done lightly.
    
    Finally, the code would be in the wrong place - not closely connected with
    the database init.

--- goosebit/db/db.py
+++ goosebit/db/db.py
@@ -1,16 +1,10 @@
 from tortoise import Tortoise, run_async
 
 from goosebit.db.config import TORTOISE_CONF
-from goosebit.db.models import Software
 
 
 async def init():
     await Tortoise.init(config=TORTOISE_CONF)
-
-    for software in await Software.all():
-        if software.local and not software.path.exists():
-            # delete it
-            await software.delete()
 
 
 async def close():
