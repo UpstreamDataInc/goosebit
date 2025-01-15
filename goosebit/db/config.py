3b56274847f6efd7400c8f89fb8a43f3b67bf8fc
Upstream Data <brett@upstreamdata.ca>
2025-01-15 15:04:37 -0700

    Allow extending database models

--- goosebit/db/config.py
+++ goosebit/db/config.py
@@ -1,10 +1,17 @@
 from goosebit.settings import config
 
+
+def add_models(models_path: str):
+    models.append(models_path)
+
+
+models = ["goosebit.db.models", "aerich.models"]
+
 TORTOISE_CONF = {
     "connections": {"default": config.db_uri},
     "apps": {
         "models": {
-            "models": ["goosebit.db.models", "aerich.models"],
+            "models": models,
         },
     },
 }
