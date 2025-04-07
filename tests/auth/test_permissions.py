aad673cb4f82eb42d4b5a9e5ca4504cf3eb555e0
Brett Rowan <121075405+b-rowan@users.noreply.github.com>
2025-04-07 08:53:24 -0600

    tests: modify tests for new permissions style

--- tests/auth/test_permissions.py
+++ tests/auth/test_permissions.py
@@ -1,29 +1,43 @@
 from goosebit.auth import check_permissions
+from goosebit.auth.permissions import GOOSEBIT_PERMISSIONS
 
 
 def test_single_permission():
-    assert check_permissions(["device.read"], ["device.read"])
+    assert check_permissions([GOOSEBIT_PERMISSIONS["device"]["read"]()], ["goosebit.device.read"])
 
 
 def test_wildcard_sub_permission():
-    assert check_permissions(["device.read"], ["device.*"])
+    assert check_permissions([GOOSEBIT_PERMISSIONS["device"]["read"]()], ["goosebit.device.*"])
 
 
 def test_root_permission():
-    assert check_permissions(["device.read"], ["device"])
+    assert check_permissions([GOOSEBIT_PERMISSIONS["device"]["read"]()], ["goosebit.device"])
 
 
 def test_root_wildcard_permission():
-    assert check_permissions(["device.read"], ["*"])
+    assert check_permissions([GOOSEBIT_PERMISSIONS["device"]["read"]()], ["*"])
 
 
 def test_multiple_single_permissions():
-    assert check_permissions(["device.read", "device.write"], ["device.read", "device.write"])
+    assert check_permissions(
+        [GOOSEBIT_PERMISSIONS["device"]["read"](), GOOSEBIT_PERMISSIONS["device"]["write"]()],
+        ["goosebit.device.read", "goosebit.device.write"],
+    )
 
 
 def test_invalid_multiple_single_permissions():
-    assert not check_permissions(["device.read", "device.write"], ["device.read", "device.read"])
+    assert not check_permissions(
+        [GOOSEBIT_PERMISSIONS["device"]["read"](), GOOSEBIT_PERMISSIONS["device"]["write"]()],
+        ["goosebit.device.read", "goosebit.device.read"],
+    )
 
 
 def test_multiple_root_wildcard_permissions():
-    assert check_permissions(["device.write", "device.read", "software.read"], ["*.read", "device.write"])
+    assert check_permissions(
+        [
+            GOOSEBIT_PERMISSIONS["device"]["write"](),
+            GOOSEBIT_PERMISSIONS["device"]["read"](),
+            GOOSEBIT_PERMISSIONS["software"]["read"](),
+        ],
+        ["goosebit.*.read", "goosebit.device.write"],
+    )
