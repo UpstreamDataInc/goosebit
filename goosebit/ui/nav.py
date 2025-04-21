d693a12361dff3eb079889d450c48cceb497c506
Brett Rowan <121075405+b-rowan@users.noreply.github.com>
2025-04-21 09:07:22 -0600

    refactor: show settings if ser has enough permissions, and allow redirecting to settings
    Added settings as a nav route with the `show=False` parameter so that we can render it on a custom location on the navbar.

--- goosebit/ui/nav.py
+++ goosebit/ui/nav.py
@@ -5,15 +5,16 @@ class NavigationItem(BaseModel):
     function: str
     text: str
     permissions: list[str]
+    show: bool
 
 
 class Navigation:
     def __init__(self):
         self.items = []
 
-    def route(self, text: str, permissions: list[str] | None = None):
+    def route(self, text: str, permissions: list[str] | None = None, show: bool = True):
         def decorator(func):
-            self.items.append(NavigationItem(function=func.__name__, text=text, permissions=permissions))
+            self.items.append(NavigationItem(function=func.__name__, text=text, permissions=permissions, show=show))
             return func
 
         return decorator
