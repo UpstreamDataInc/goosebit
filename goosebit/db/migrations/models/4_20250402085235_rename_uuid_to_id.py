9eba504bab82a605442ded80553a8aa1d6c0beed
Brett Rowan <121075405+b-rowan@users.noreply.github.com>
2025-11-17 08:32:44 -0700

    bug: upgrade aerich migrations

--- goosebit/db/migrations/models/4_20250402085235_rename_uuid_to_id.py
+++ goosebit/db/migrations/models/4_20250402085235_rename_uuid_to_id.py
@@ -1,5 +1,7 @@
 from tortoise import BaseDBAsyncClient
 
+RUN_IN_TRANSACTION = True
+
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
@@ -9,3 +11,42 @@ async def upgrade(db: BaseDBAsyncClient) -> str:
 async def downgrade(db: BaseDBAsyncClient) -> str:
     return """
         ALTER TABLE "device" RENAME COLUMN "id" TO "uuid";"""
+
+
+MODELS_STATE = (
+    "eJztW+tz2jgQ/1cYPuVmch1eeVy/kcRJuBLTAXPttOl4FKyAJ0aittyU6+R/P0l+Sn6cDQ"
+    "RMw5cE9mGkn1a7q135V32ODWg5767gD3MC6+9rv+oIzNkHiXNcq4PFIqIzAgEPFhc1IpkH"
+    "h9hgQij1EVgOPGZMZ2KbC2JiRKnItSxGxBMqaKJpRHKR+d2FOsFTSGbQpoyv3yjZRAb8CZ"
+    "3g6+JJfzShZQhDNQ3225yuk+WC0y5nwL7mkuznHvQJttw5iqQXSzLDKBSno2HUKUTQBgQa"
+    "sQmw8fkzDUjeWCmB2C4MB2lEBAM+AtcisQkXRGGCEUPQRMThU5yDn7oF0ZTM6NfWycmLN5"
+    "torp4Ym8I/3eHlbXd4RKX+YHPBdCG89VF9VsvjvfCHAAK8x3BsIzD5/xJwBvIrAerDFeIZ"
+    "iESARmZUcUQjBB+xPYG6u6AQpyB5gbEFAUoHU1aVQH2guquYaRFYy+5XCdccGC8Ggz4b9d"
+    "xxvluc0NMkOMd3F8rwqMlRpkIm4eSeqknQOs/6D2g7bGwlTFTUOhhqaKgQlvKcgfzWEAw/"
+    "VhpGb7vqLDAm0ewhoiB3zgHt0ZEB5EVKAVjpCRK+dCqvtenbCWDr6kBV3tca96jf1ZSR9r"
+    "7WvEcfe6qqXFH47tFw0O8PxpTcvkfd0ah3wxmderHFmLKB/dlunZ2eUy4fN/tylrMoo7tu"
+    "v5/0BBZwiE7hTHOxhTAXH7A9yJs5kI/VD+rgk8oxHyo3vZGmDAPcx6raU2847spwOBhS0O"
+    "/RdU/tjW6ZyMnWV2Bh46kNHScV/3QHEldZCfEduGEPsFazc9Y5b592QtRCSh50GXZr4WkS"
+    "NQ3+zIAtrrMn0SsHE035rAnpQOBcj+66n7nfnS99Tn+g3gTiMWd82R9cpLoDCFPyggtzmm"
+    "mQgtp+WeRfrVa7fdZqtE/PTzpnZyfnjdA0k6w8G73o3TAzFSDOsFtzUSZdiKnsidWKyUKz"
+    "SK7QzE4VmolMwUfkx+kKMHpKewlkp1EAyE4jE0jGEoEELpnpBD+l7fdsJEWtvYSy3SoAZb"
+    "uVCSVjSVA6jjlF0NAd/EiegQ31tHJKpgfNUt8vZ7qx8E5tzygPoqS1vWR09+Cxot7jU2ol"
+    "KmFaSUSvsQ2pzAe4TCT7Eoh+OXMUe1TljPElsIuAGo3CBs9h4TNzz9Hp0klCr3gyUrSaOu"
+    "7366n2uQEob2OPqp5tFsVS2noChJfd0WX3SqlzI30AkycqaOiCtTIObmGJEsomWfPWPNXY"
+    "CZimnKTuAFpqmP0tuCgamFaxjp2zGnzgutR18KdhQ4tNIuR6jQYPJ2xzhJ/g0ofPX8EQe5"
+    "/j6fhMMrOxO53F6AHuqQtP6XoCSm4Nc4DAlNPYhF+Ok7sipZcS3zHZ3ZT4Fq1OPyUzdhUN"
+    "Wf5y77SbspFgn9064YtZJiENFTbTjfotitI23ZdlC/xxnTcOZSKfyo5dEeZxvyrWTnzF6w"
+    "9D5orTIU50iqsHdlb8edlY/A5SsU0E8RJJauUjeXwucjif4PmCGtWDaZlkmRLUpfRWjuxS"
+    "6haL7aFi4hcKhfnQMjKj/BBbFnZJWpAPWLkx3o4JHUL8HoX4iQ25CQOSxO+Kcog5h+kgip"
+    "oSmIav+i74UE03SncwMAbIWgabJBtKrXenjLTu3Ueh1H/V1RTGaQll/oB6dCpFt/AhtU89"
+    "7bbGvta+DFS+YxfYIayZJMhpX9hxlxX6sI7wsw6MmI0F1AAYIe04XHtZN3Hb6m2ClYx/P6"
+    "4TLIDrpCGZe3EoUjpcGZKuDLkTmto6FCsXpTjtzKCX0NteVbax6/gX29TAtFyeSJVDL6H3"
+    "JtFbraWyiU5KAeQqmHnlNAN20wPYPoxF69Z5pf/Xr1vnHo5CfFNOR3Hss49H8cU+nI+qtk"
+    "uPc85Hrm2WyQF98TdesxNChvlv2qXxnDtMgcaedY23fH9pBpxZGcsM5A+mGUK4ws37da/d"
+    "/z5ArlSXDxv92y3QV+fmg9gZ8oqma6IQq89Wz/hev0+RrLmv16tY5xZI1XoV8bnIvQqxvy"
+    "P2KaRGhNynkFL1zfUpClxHYBcpUtJw/35FdgZOfIFD8r1HyfdWa9hvMjRvzA1nBvTSDnj1"
+    "tnvV3G80E9n5hpfjRL8rXO2SvW50I2zNS1//3w0eO9y/Jbwsp+e6WTeQ2ImfZb9e1mfEdQ"
+    "7v0Gc4YnZ4pOa7eC7VEBO13rhTFjpi0J6bDjtIpjjMv0cDNaMnJqpJgI4RZXw1zAk5rlmm"
+    "Q769Wtfx67c1Go45aLKZC02yxIt08jtzx2LbnD1AfpEOIjbXsu3HmNYW+4+ho6hY+3F7CU"
+    "VuWOpC25zM0gKTz8kNTSCSORwCNukvX/kQcKjSbSjqsK1RAkRffD8BbDaKvKJJpbJfdm0k"
+    "XtKkv0hg2q2B7JAdU9lVuN7ImedVw/VOw8vLfxzFe10="
+)
--- goosebit/db/migrations/models/6_20250904081506_add_image_format.py
+++ goosebit/db/migrations/models/6_20250904081506_add_image_format.py
@@ -1,11 +1,54 @@
 from tortoise import BaseDBAsyncClient
 
+RUN_IN_TRANSACTION = True
+
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
-        ALTER TABLE "software" ADD "image_format" SMALLINT NOT NULL  DEFAULT 0 /* SWU: 0\nRAUC: 1 */;"""
+        ALTER TABLE "software" ADD "image_format" SMALLINT NOT NULL  DEFAULT 0 /* SWU: 0
+RAUC: 1 */;"""
 
 
 async def downgrade(db: BaseDBAsyncClient) -> str:
     return """
         ALTER TABLE "software" DROP COLUMN "image_format";"""
+
+
+MODELS_STATE = (
+    "eJztXOtz2jgQ/1cYPuVmch0eeV2+kYQkXInpgLl22nQ8ClbAE1uitpyU6+R/P0l+Sn6cTX"
+    "iYhi8NrHaN9NNqd7W77q+6hXVoOh+u4LMxgfXz2q86Ahb7II0c1upgPo/ojEDAg8lZ9Yjn"
+    "wSE2mBBKfQSmAw/ZoDOxjTkxMKJU5JomI+IJZTTQNCK5yPjhQo3gKSQzaNOBb98p2UA6/A"
+    "md4Ov8SXs0oKkLUzV09tucrpHFnNMuZ8C+5pzs5x60CTZdC0Xc8wWZYRSy09kw6hQiaAMC"
+    "9dgC2Pz8lQYkb66UQGwXhpPUI4IOH4FrktiCC6IwwYghaCDi8CVa4KdmQjQlM/q1dXz86q"
+    "0mWqvHxpbwT2d4edsZHlCuP9haMN0Ib38Uf6jljb3yhwACvMdwbCMw+d8ScAb8SwHqwxXi"
+    "GbBEgEZqVHFEIwQfsT2BmjunEKcgeYGxCQFKB1MWlUB9oLLLqGkRWMueVwnXHBgvBoM+m7"
+    "XlOD9MTuipEpzju4vu8KDJUaZMBuHknqJK0Dov2jO0HTa3EioqSu0VNVRUCEtZzoB/YwiG"
+    "HysNo3dcNeYYk2j2EOki1+KA9ujMAPI8pQCs9AQJX7qUdR36dgLYujJQuue1xj3qd9TuSD"
+    "2vNe/Rp56idK8ofPdoOOj3B2NKbt+jzmjUu+EDR/VimzFlE/uz3To9OaOjfN7sy2nOpozu"
+    "Ov1+0hKYwCEahTPNxBbCXHzA5iBv5kA+Vj4qg88Kx3zYvemN1O4wwH2sKD3lhuPeHQ4HQw"
+    "r6PbruKb3RLWM53vgOzG08taHjpOKfbkDiIkshvgUz7AHWah6dHp21T45C1EJKHnQZemvi"
+    "aRI1Ff7MgC0usyPeKwcTtftFFcKBwLge3HW+cLtrLfyR/kC5CdhjxviyP7hINQcQpsQFF8"
+    "Y0UyEFsd3SyL9arXb7tNVon5wdH52eHp81QtVMDuXp6EXvhqmpAHGG3hrzMuFCTGRHtFYM"
+    "FppFYoVmdqjQTEQKPiLPJ0vA6AntJJBHjQJAHjUygWRDIpDAJTON4Ke0856NpCi1k1C2Ww"
+    "WgbLcyoWRDEpSOY0wR1DUHP5IXYEMtLZ2SaUGzxHfLmK7MvVPd08uDKEltLhjdPngsqff4"
+    "lJqJSqhWEtFrbEPK8xEuEsG+BKKfzhzFHlU5ZXwN9CKgRrOwwUuY+Mw8c3S5dJHQS56Mum"
+    "pNGff79VT9XAGUt7FHVU83i2IpHT0BwsvO6LJz1a1zJX0AkyfKqGuCtrIR3MISJeRNDlkt"
+    "K1XZCZim3KTuAFqomP1bcFNUMK1iHjtnN/jENanq4C/DhiZbRDjqFRo8nLDNEX6CCx8+fw"
+    "dD7P0RT8YfJDMbu9NZjB7gnrrxlK4loOTaYAEEppzGFvx6mDwVKbWU+InJrqbEj2h16imZ"
+    "vquoy/K3e6vVlJU4++zSCd/MMgFpKLCaatRvkZS26bksm+CPy7xzKBPxVLbvijCP21Uxd+"
+    "ILXn8cMlOcDnGiUlw9sLP8z+vK/HcQiq3CiZcIUivvyeNrkd35BFtzqlQPhmmQRYpTl8Jb"
+    "2bNLoVvMt4eCiV8o5OZDzcj08kNsmtglaU4+GMr18XaMae/id8jFT2zIVRiQJH5XdIQYFk"
+    "wHUZSUwNR90Q/Bh2qaUXqCgT5A5iI4JNlQqr277kjt3H0SUv1XHbXLRlpCmj+gHpxI3i18"
+    "SO1zT72tsa+1rwOFn9g5dggrJgl86ld23WWJPqwh/KIBPaZjATUARgg79m0vbw3cNtpNsJ"
+    "Ty70Y7wRy4ThqSuY1DkdC+ZUhqGXInNLR1KFYuSjHamU4vIbe5rGxj2/4vdqiBYbo8kCqH"
+    "XkLuXaK3XEllFZWUAshVMPLKKQZspwaweRiL5q3zUv/rz1vnXo5CfFNuR3Hss69H8c3e34"
+    "+qdkoPc+5Hrm2UiQF99neesxNchvFvWtN4Tg9TILFjVeMN9y/NgDMro5kB/141QwiX6Lx/"
+    "a9v97wmkYVHHqT1i20pLIxXqXpYfsc3wuj76PObty8PO+PK81qyXMQPLdiIvVecIGyc2W/"
+    "CoTieJWGnzktBvRCGW767eYV5/3SdZw3hb7ectXTVVq/3E1yLXfsR6mVj3kQo7ct1Huvqs"
+    "ru5ToL2DNaakXGv8fpXsGw3xGfaXmdVHkmu7zGy0JvD7hDolXPPKzHCmQy9tgJdvY6ia+Y"
+    "1WIhvfsNlQtLtCq5xsdaMOuzc20f1/dX3scPuWsLKcnmtm3YBjK3aW/XpZmxGX2f+fBBmG"
+    "mF3GqfrOX0oVGEWpd26UhQojtC3DYRfzFIP592igZNQYRTEJ0DGiA990Y0IOa6bhkO9rq+"
+    "J++/6GAm4OmmzlQtEx8WKi/A7iodiGwB4gv5gIEVtr2XJuTGqD9dzQUFSsnLu5gCLXLXWg"
+    "bUxmaY7JH8l1TSDi2V8CVmkv13wJ2Gc9V+R12NEoAaLPvpsANhtFXnmlXNkvDzcSL73SXy"
+    "QwrQsj22XHRLblrldy51mru96qe3n9Dz/45b0="
+)
